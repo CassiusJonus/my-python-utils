6ff830d6c487e66a96aca1e0cb048873fb70c79d
Cassius Jonus <vegetafan5@gmail.com>
2022-03-05 19:30:22 -0600

    called load_dotenv() function to load the environment variables

--- src/custom_utils/message_notifier.py
+++ src/custom_utils/message_notifier.py
@@ -64,6 +64,7 @@ def test():
     logging_level = logging.WARNING
     app_logger = logging.getLogger(__name__)
     app_logger.setLevel(logging_level)
+    load_dotenv()
     try:
         send_notification("7084751390@tmomail.net",
                           "Test Message", "Hello phone!")
